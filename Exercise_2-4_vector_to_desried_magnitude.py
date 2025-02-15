8d04fe3d70f464b69d44610c4547413e610a61ea
rowan01g <140080596+rowan01g@users.noreply.github.com>
2025-02-15 16:50:58 +0000

    create function that allows user to scale vector to desired magnitude

--- Exercise_2-4_vector_to_desried_magnitude.py
+++ Exercise_2-4_vector_to_desried_magnitude.py
@@ -4,11 +4,12 @@ import numpy as np
 #fisrt calculate the unit vector and then scalar multiply this by desired magnitude from user input 
 
 def create_unit_vector(v):
+    m = input('Please input a magnitude to scale your vector: ')
     v_norm = np.linalg.norm(v)
     print(f'The Euclidean norm of your array is: {v_norm}')
 #scalar multiplcation of v with the reciprocal norm to get the unit vector
-    v_hat = v/v_norm
-    print(f'The magnitude of the vector is now: {v_hat}')
+    v_hat = v/v_norm*int(m)
+    print(f'Scaled to your value of {3}, the magnitude of the vector is now: {v_hat}')
 
 array1 = np.array([3,4])
 create_unit_vector(array1)
