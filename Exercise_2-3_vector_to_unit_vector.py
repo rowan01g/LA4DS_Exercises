2727c54ae4dee29862998e89119fdf2f35578328
rowan01g <140080596+rowan01g@users.noreply.github.com>
2025-02-15 16:23:49 +0000

    create file and fuction to calculate unit vector from input vector

---
+++ Exercise_2-3_vector_to_unit_vector.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+# any nonunit vector has an unit vector associated unit vector. A unit vector has a geometric length (magnitude) of 1 - if associated with a nonunit vecotr, it will be in the same direction
+
+def create_unit_vector(v):
+    v_norm = np.linalg.norm(v)
+    print(f'The Euclidean norm of your array is: {v_norm}')
+#scalar multiplcation of v with the reciprocal norm to get the unit vector
+    v_hat = 1/v_norm*v
+    print(f'The magnitude of the vector is now: {v_hat}')
+
+array1 = np.array([ [1,2],
+                    [3,4]
+])
+create_unit_vector(array1)
+
+
+    
