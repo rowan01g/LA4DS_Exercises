f880086a8d01ccadbd3cdb160d1bff1000911ca0
rowan01g <140080596+rowan01g@users.noreply.github.com>
2025-03-18 14:35:42 +0000

    matrix rank

--- Exercise_6-5_matrix_rank.py
+++ Exercise_6-5_matrix_rank.py
@@ -1,6 +1,19 @@
 #%%
+
 import matplotlib.pyplot as plt
 import numpy as np 
-``````````
 
-# %%
+M = 10
+N = 20 
+r = 15
+
+m1 = np.random.randn(M,r)
+m2 = np.random.randn(r,N)
+
+m1m2 = m1@m2
+
+print(f'The rank of the matrix m1m2 is: {np.linalg.matrix_rank(m1m2)}')
+print(np.shape(m1m2))
+
+
+#%%
